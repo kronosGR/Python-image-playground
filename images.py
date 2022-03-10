9ec72b071d1a055c963c45bbbf66eb4debd5a984
KronosGR <geo.elgeo@gmail.com>
2022-03-10 20:07:28 +0100

    more testing with Pillow

--- images.py
+++ images.py
@@ -2,5 +2,14 @@ from PIL import Image, ImageFilter
 
 img = Image.open('./Pokedex/pikachu.jpg')
 filtered_img = img.convert('L')
-
 filtered_img.save("grey.png", "png")
+
+crooked = filtered_img.rotate(90)
+crooked.save('crooked.png', 'png')
+
+box = (100, 100, 400, 400)
+crop = filtered_img.crop(box)
+crop.save('crop.png', 'png')
+
+resize = filtered_img.resize((300, 300))
+resize.save('resized.png', 'png')
